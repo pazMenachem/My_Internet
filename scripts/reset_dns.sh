606151a8846952d51ab3949e1c486596a2bf1dc5
elipaz <paz.menach@gmail.com>
2024-12-02 15:06:00 +0200

    Add install and active scripts.
    Add important line for kernel networking process.

--- scripts/reset_dns.sh
+++ scripts/reset_dns.sh
@@ -3,6 +3,9 @@
 # Flush all NAT table rules
 sudo iptables -t nat -F
 
+# Disable routing for localhost after the script completes
+sudo sysctl -w net.ipv4.conf.all.route_localnet=0
+
 echo "DNS redirection rules have been reset."
 
 # Display the current (empty) rules
